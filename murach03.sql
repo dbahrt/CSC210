9e268db48400333d4a4e5f9391999a931422595f
Dan Bahrt <28609248+dbahrt@users.noreply.github.com>
2019-10-17 12:03:37 -0700

    minor format changes

--- murach03.sql
+++ murach03.sql
@@ -1,6 +1,6 @@
 -- murach03.sql
 
-/*
+/*==========
 source:  Syverson, Murach's SQL Server 2016, p. 89.
 description: A SELECT statement that retrieves all the data from the Invoices table:
 */
@@ -12,12 +12,12 @@ description: A SELECT statement that retrieves all the data from the Invoices ta
 
 /* 
 analysis:  The SELECT * clause tells SQL to work with all of the columns in the 
-specified table.  The FROM clause specifies what table to use, in this case we'll use 
-the Invoices table.  This is pretty much as simple as a query gets.  The SELECT 
-clause specifies a list of expressions in terms of columns that we want to include 
-in our query.  We found that its a good idea to explicitly specify which database 
-we want to use with our query. Our text suggests we should always make sure to
-specify the database we are using. 
+specified table.  The FROM clause specifies what table to use, in this case 
+we'll use the Invoices table.  This is pretty much as simple as a query gets.  
+The SELECT clause specifies a list of expressions in terms of columns that 
+we want to include in our query.  We found that its a good idea to explicitly 
+specify which database we want to use with our query. Our text suggests 
+we should always make sure to specify the database we are using. 
 */
 
 /* sample output:
@@ -26,10 +26,11 @@ copy and paste the output from this query here!
 
 */
 
-/*
-Syverson, Murach's SQL Server 2016, p. 89.
 
-A SELECT statement that retrieves three columns from each row, sorted In ascending sequence by Invoice total:
+/*==========
+source:  Syverson, Murach's SQL Server 2016, p. 89.
+description: A SELECT statement that retrieves three columns from each row, 
+sorted In ascending sequence by Invoice total:
 */
 
     USE AP;
@@ -39,7 +40,7 @@ A SELECT statement that retrieves three columns from each row, sorted In ascendi
     ORDER BY InvoiceTotal;
 
 
-/* explanation:
+/* analysis:
 */
 
 /* sample output:
